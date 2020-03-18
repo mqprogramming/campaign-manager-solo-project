d7e8ea6d5f15115b6f0db4e5bf6a797ebc7579c0
mqprogramming <matt.qgley@gmail.com>
2020-03-18 15:52:45 +0000

    Added new pages

--- controllers/character_controller.rb
+++ controllers/character_controller.rb
@@ -17,12 +17,39 @@ end
 
 # New
 get '/characters/new' do
-  erb(:"characters/new")
+  erb(:"characters/new/new")
 end
 
-# Create
-post '/characters' do
+# Create - Page Two
+post '/characters/newpg2' do
+  @@parameters1 = params
+  erb(:"characters/new/newpg2")
+end
 
+# Create - Page Three
+post '/characters/newpg3' do
+  @@parameters2 = @@parameters1.merge(params)
+  erb(:"characters/new/newpg3")
+end
+
+# Create - Page Four
+post '/characters/newpg4' do
+  @@parameters3 = @@parameters2.merge(params)
+  erb(:"characters/new/newpg4")
+end
+
+# Create - Page Five
+post '/characters/newpg5' do
+  @@parameters4 = @@parameters3.merge(params)
+  erb(:"characters/new/complete")
+end
+
+# Create - Complete
+post '/characters' do
+  full_params = @@parameters4.merge(params)
+  @new_character = Character.new(full_params)
+  @new_character.save()
+  erb(:"characters/created")
 end
 
 # Show
