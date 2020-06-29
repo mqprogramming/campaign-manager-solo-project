beec9638be4b4cda2936fd347e3d62273fe7788a
mqprogramming <matt.qgley@gmail.com>
2020-06-29 19:35:14 +0100

    Added Bootstrap styling, improved CSS

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require('sinatra/twitter-bootstrap')
 
 require_relative('controllers/campaign_controller.rb')
 require_relative('controllers/character_controller.rb')
+
+class TestApp < Sinatra::Base
+  register Sinatra::Twitter::Bootstrap::Assets
+end
 
 get '/' do
   erb( :index )
