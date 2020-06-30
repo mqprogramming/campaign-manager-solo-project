53b7ba57cc48113b82c3cf1f7fc484683ad5dc1f
Matt Q <matt.qgley@gmail.com>
2020-06-30 16:39:10 +0100

    db switch

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -6,9 +6,9 @@ class SqlRunner
 
     begin
       # LOCAL HOSTING
-      db = PG.connect( {dbname: 'campaign_manager', host: 'localhost'} )
+      # db = PG.connect( {dbname: 'campaign_manager', host: 'localhost'} )
       # ONLINE HOSTING
-      # db = PG.connect( {dbname: 'dekam0n2osrq0b', host: 'ec2-50-19-26-235.compute-1.amazonaws.com', port: 5432, user: 'sbttxusjlraoun', password: 's2e834f7196a0102cb3b64a5b05cf120ab09dd95a894693a500e6d3402cc366e8'} )
+      db = PG.connect( {dbname: 'dekam0n2osrq0b', host: 'ec2-50-19-26-235.compute-1.amazonaws.com', port: 5432, user: 'sbttxusjlraoun', password: 's2e834f7196a0102cb3b64a5b05cf120ab09dd95a894693a500e6d3402cc366e8'} )
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
