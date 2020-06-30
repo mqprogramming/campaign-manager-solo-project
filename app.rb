da8b022b036a15bd589ef00765cbd77f87f92b3d
Matt Q <matt.qgley@gmail.com>
2020-06-30 16:37:29 +0100

    Removed unused gems

--- app.rb
+++ app.rb
@@ -1,13 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
-require('sinatra/twitter-bootstrap')
 
 require_relative('controllers/campaign_controller.rb')
 require_relative('controllers/character_controller.rb')
-
-class TestApp < Sinatra::Base
-  register Sinatra::Twitter::Bootstrap::Assets
-end
 
 get '/' do
   erb( :index )
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,8 +5,10 @@ class SqlRunner
   def self.run(sql, values = [])
 
     begin
-      # db = PG.connect( {dbname: 'campaign_manager', host: 'localhost'} )     LOCAL HOSTING
-      db = PG.connect( {dbname: 'dekam0n2osrq0b', host: 'ec2-50-19-26-235.compute-1.amazonaws.com', port: 5432, user: 'sbttxusjlraoun', password: 's2e834f7196a0102cb3b64a5b05cf120ab09dd95a894693a500e6d3402cc366e8'} )
+      # LOCAL HOSTING
+      db = PG.connect( {dbname: 'campaign_manager', host: 'localhost'} )
+      # ONLINE HOSTING
+      # db = PG.connect( {dbname: 'dekam0n2osrq0b', host: 'ec2-50-19-26-235.compute-1.amazonaws.com', port: 5432, user: 'sbttxusjlraoun', password: 's2e834f7196a0102cb3b64a5b05cf120ab09dd95a894693a500e6d3402cc366e8'} )
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
